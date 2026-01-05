eece5d211d1174e020514251256da043cfe45461
Juraj Mudroch <mudroch.juraj@protonmail.com>
2026-01-05 12:23:15 +0100

    added hero

--- pages/index.vue
+++ pages/index.vue
@@ -53,9 +53,8 @@ onMounted(async () => {
       loop
       playsinline
       preload="auto"
-      poster="/videos/poster.jpg"
     >
-      <source src="/videos/hero-4k-ultra.mp4" type="video/mp4">
+      <source src="/videos/kacenkovo_web_hd.mp4" type="video/mp4">
     </video>
     <div class="hero__overlay" />
     <div class="hero__content">
