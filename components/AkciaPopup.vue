aeca985cfd92a5ced1b742becd46142815965fb8
Juraj Mudroch <mudroch.juraj@protonmail.com>
2026-04-17 18:08:23 +0200

    update

--- components/AkciaPopup.vue
+++ components/AkciaPopup.vue
@@ -66,7 +66,7 @@ onUnmounted(() => {
             </svg>
           </button>
           <img
-            src="/akcie/akcia_2722026.webp"
+            src="/akcie/akcia_17042026.webp"
             alt="Aktuálna akcia"
             class="akcia-popup__image"
             fetchpriority="high"
